dedbdbfc5a983a2bc2fca689ebc0af8a8fba7495
dib3474 <dib3474@gmail.com>
2025-12-06 16:44:07 +0900

    feat: 7 segment 재수정

--- Display_unit.v
+++ Display_unit.v
@@ -132,8 +132,8 @@ module Display_Unit (
                     3'd2: seg_1_data = 8'b1101_1010; // 2
                     3'd3: seg_1_data = 8'b1111_0010; // 3
                     3'd4: seg_1_data = 8'b0110_0110; // 4
-                    3'd5: seg_1_data = 8'b0110_1101; // 5
-                    3'd6: seg_1_data = 8'b0111_1101; // 6
+                    3'd5: seg_1_data = 8'b1011_0110; // 5
+                    3'd6: seg_1_data = 8'b0011_1110; // 6
                     default: seg_1_data = 8'b0000_0000;
                 endcase
             end else begin
